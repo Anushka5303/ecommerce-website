b66afd8b2bd62c94d03be37dc0a027ccabe7c6b9
anushka <anushka20gupta03@gmail.com>
2024-07-30 22:21:21 +0530

    6:00:00

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,25 @@
 import express from 'express'
 import {isAdmin, requireSignIn} from './../middlewares/authMiddleware.js';
-import { createCategoryController } from '../controllers/categoryController.js';
+import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from '../controllers/categoryController.js';
 
 const router = express.Router()
 
 //routes
+
+//create Category
 router.post('/create-category',requireSignIn,isAdmin,createCategoryController)
+
+
+//update Category
+router.put('/update-category/:id',requireSignIn, isAdmin, updateCategoryController)
+
+//getAll category
+router.get('/get-category',categoryController)
+
+//sinle category
+router.get('/single-category/:slug',singleCategoryController)
+
+//delete category
+router.delete('/delete-category/:id',requireSignIn, isAdmin, deleteCategoryController)
 
 export default router  
